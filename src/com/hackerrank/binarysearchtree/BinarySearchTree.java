1ca1012eea6b22741070f5fd8ca6d8c248d06def
Anurag Misra <MishRanu@New-mac-mini.local>
2017-10-12 21:32:38 +0530

    binary search tree problem

--- src/com/hackerrank/binarysearchtree/BinarySearchTree.java
+++ src/com/hackerrank/binarysearchtree/BinarySearchTree.java
@@ -21,32 +21,57 @@ public class BinarySearchTree {
             this.right = right;
         }
     }
+    BinarySearchTree(){
+        root = null;
+    }
 
     public void makeBinarySearchTreeEmpty(){
         root = null;
     }
 
     public void insert(int data) {
+        Node node = new Node(data, 0, null, null);
+
         if (Objects.isNull(root)) {
-            Node temp = new Node(data, 0, null, null);
-            root = temp;
+            root = node;
             return;
         }
         Node nodeNext = root;
         Node nodePrevious = null;
         while (nodeNext != null) {
             if (data > nodeNext.data) {
-                nodePrevious
+                nodePrevious= nodeNext;
+                nodeNext.size+=1;
                 nodeNext = nodeNext.right;
             } else {
+                nodePrevious = nodeNext;
+                nodeNext.size+=1;
                 nodeNext = nodeNext.left;
             }
         }
+        if(data> nodePrevious.data){
+            nodePrevious.right = node;
+        }
+        else nodePrevious.left = node;
+
     }
 
 
+    public void inorderTraversal(Node node){
+        if(node.left!=null)
+            inorderTraversal(node.left);
+        System.out.println(node.data);
+        if(node.right!=null)
+            inorderTraversal(node.right);
+    }
+
     public static void main(String[] args){
-
-
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(4);
+        bst.insert(6);
+        bst.insert(5);
+        bst.insert(1);
+        bst.insert(2);
+        bst.inorderTraversal(bst.root);
     }
 }
